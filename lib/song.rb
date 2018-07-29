dc760e0d3cb754f6404f0d325e93a2bd9e075022
Sheena Bonilla <sheena.myb@gmail.com>
2018-07-29 12:12:22 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,7 +33,7 @@ class Song
     #keys => names of each genre
     #values => number of songs that have that genre
     
-    @@genres.group_by {|key| key}
+    @@genres.group_by.count {|key| key}
     @@genres.group_by {|value| value}
   end
   
