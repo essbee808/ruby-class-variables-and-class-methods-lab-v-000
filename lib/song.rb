e54e3c9edf5727d431498324aadbc0eed8a93b3d
Sheena Bonilla <sheena.myb@gmail.com>
2018-07-29 11:54:17 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,7 +31,7 @@ class Song
   
   def self.genre_count
     histogram = Hash.new { |h,k| h[k] = [] }   # hash that defaults values to empty Arrays
-ages.each {|name,age| histogram[age] << name }
+    ages.each {|name,age| histogram[age] << name }
   end
 
 end
