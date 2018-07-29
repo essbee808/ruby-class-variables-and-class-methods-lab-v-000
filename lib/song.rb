c8cd1dcecfcd09756d9ffce5806f20f41921bfff
Sheena Bonilla <sheena.myb@gmail.com>
2018-07-29 10:57:13 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,7 +31,7 @@ class Song
   
   def self.genre_count
    
-    hash = Hash[@@genres.collect {|item| [item, @@genres.count] } ]
+    hash = Hash[@@genres.collect {|item| [item, ] } ]
     
   end
 
