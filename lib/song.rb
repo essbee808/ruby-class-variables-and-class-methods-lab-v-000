047c901017c6ebdf9805b73ad6ec91588ae59bb1
Sheena Bonilla <sheena.myb@gmail.com>
2018-07-29 11:29:21 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,9 +31,11 @@ class Song
   def self.genre_count
    genre_hash = Hash.new(0)
    
-   @@genre.each {|item| genre_hash(item) += 1}
+   @@genres.each {|item| genre_hash(item) += 1}
    
-   @@genre = @@genre.sort_by {|item, amount| item.length}
+   @@genres = @@genres.sort_by {|item, amount| item.length}
+   
+   @@genres.
   end
 
 end
