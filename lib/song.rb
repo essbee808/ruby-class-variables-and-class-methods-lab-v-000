fda8e476e93d7be75dd3d2cdf5260cd8d71868a9
Sheena Bonilla <sheena.myb@gmail.com>
2018-07-29 10:54:48 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,38 @@
+class Song
+  attr_accessor :name, :artist
+  attr_reader :genre
+  
+  # class variables
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    
+    @@count += 1
+    @@artists << @artist
+    @@genres << @genre
+  end
+
+  def self.count
+    @@count
+  end
+  
+  def self.artists
+    @@artists.uniq #=> uniq method removes duplicate elements
+  end
+  
+  def self.genres
+    @@genres.uniq
+  end
+  
+  def self.genre_count
+   
+    hash = Hash[@@genres.collect {|item| [item, genre_count] } ]
+    
+  end
+
+end
