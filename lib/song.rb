0db8100176479f9eaa8ad059ff12ce0cf63fbba4
Sheena Bonilla <sheena.myb@gmail.com>
2018-07-29 10:59:51 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,7 @@ class Song
     @@genres.uniq
   end
   
-  def self.genre_count
+  def self.genre_count(genre)
    
     hash = Hash[@@genres.sort.collect {|item| [item, ] } ]
     if @genre == genre
